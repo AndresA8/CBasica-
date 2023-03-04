3373e7ca5e9da96ae92f3a073b451d0e0b629564
edison lopez <stiiven6241@gmail.com>
2023-03-04 10:29:15 -0500

    Maquetacion de calculadora con react native

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container,{flex:1}]}>
+      <View style={[styles.container,styles.views,{flex:1,backgroundColor:'powderblue',}]}>
+        <Banner name='calcu'></Banner>
+      </View>
+
+      <View style={[styles.container,styles.views,{flex:5,backgroundColor:'yellowgreen',}]}>
+      <Text>Cuerpo</Text>
+      </View>
+
+      <View style={[styles.container,styles.views,{flex:1,backgroundColor:'gray',}]}>
+      <Text>Derechos Reservados</Text>
+      </View>
     </View>
+  );
+}
+
+//Crear componente para la imagen
+function Banner(props){
+  return(
+    <Image source={require(`./assets/images/${props.name}.jpg`)} style={{width:'100%',height:'100%',resizeMode:'stretch'}}>
+
+    </Image>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  views:{
+    width:'100%',
+    heigth:'100%',
+    borderColor:'black',
+    borderWidth:2,
+    borderRadius:5
+  }
 });
